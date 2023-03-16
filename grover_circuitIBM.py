94d2bd667dafdddc98320e5a0e4a3dd594ed2bdc
javierrome236 <javierromero236@gmail.com>
2023-03-16 08:26:56 +0000

    Update from https://github.com/javierrome236/quantumDeployment/commit/5a64ae9c7cb472abbfa6dddaa3e9a46e401e6f06

---
+++ grover_circuitIBM.py
@@ -0,0 +1,21 @@
+from qiskit import execute, QuantumRegister, ClassicalRegister, QuantumCircuit, Aer
+from numpy import pi
+qreg_q = QuantumRegister(5, 'q')
+creg_c = ClassicalRegister(5, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[2])
+circuit.h(qreg_q[3])
+circuit.h(qreg_q[4])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])
+circuit.measure(qreg_q[3], creg_c[3])
+circuit.measure(qreg_q[4], creg_c[4])
+backend = Aer.get_backend("qasm_simulator")
+x=int(shots)
+job = execute(circuit, backend, shots=x)
+result = job.result()
+counts = result.get_counts()
+return counts
---
+++ shor_circuitAWS.py
@@ -0,0 +1,38 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(4, 'q')
+creg_c = ClassicalRegister(4, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.reset(qreg_q[0])
+circuit.reset(qreg_q[1])
+circuit.reset(qreg_q[2])
+circuit.reset(qreg_q[3])
+circuit.x(qreg_q[3])
+circuit.x(qreg_q[0])
+circuit.x(qreg_q[1])
+circuit.x(qreg_q[2])
+circuit.x(qreg_q[3])
+circuit.x(qreg_q[0])
+circuit.x(qreg_q[2])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.cx(qreg_q[2], qreg_q[1])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.cx(qreg_q[1], qreg_q[0])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.cx(qreg_q[0], qreg_q[3])
+circuit.cx(qreg_q[3], qreg_q[0])
+circuit.cx(qreg_q[0], qreg_q[3])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])
+circuit.measure(qreg_q[3], creg_c[3])
+
+backend = Aer.get_backend("qasm_simulator")
+x=int(shots)
+job = execute(circuit, backend, shots=x)
+result = job.result()
+counts = result.get_counts()
+return counts
